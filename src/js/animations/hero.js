2c0e9510519aec77e778a40979898276fc915e95
LazarusThe3rd <alazar.kebede@student.arteveldehs.be>
2026-06-04 00:25:48 +0200

    Add first ScrollTrigger reveal animation🎬

--- src/js/animations/hero.js
+++ src/js/animations/hero.js
@@ -1,9 +1,20 @@
 import { gsap } from "../gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export function heroAnimation() {
   gsap.from(".hero h1", {
     opacity: 0,
     y: 100,
     duration: 1.5,
+  });
+
+  gsap.from(".reveal-section h2", {
+    opacity: 0,
+    y: 100,
+
+    scrollTrigger: {
+      trigger: ".reveal-section",
+      start: "top 80%",
+    },
   });
 }
